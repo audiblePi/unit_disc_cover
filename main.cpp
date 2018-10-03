85dd6286ef934f16f3b8621993ec45ec55a4b290
B Hicks <b@audiblePi.com>
2018-10-03 17:55:53 -0400

    Moved to class implementation

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 //Unit Disk Cover - Brute Force
-#include <CGAL/Simple_cartesian.h>
 #include <cassert>
 #include <vector>
 #include <algorithm>
+
+#include <CGAL/Simple_cartesian.h>
 #include <CGAL/point_generators_2.h>
 #include <CGAL/algorithm.h>
 #include <CGAL/random_selection.h>
+#include <CGAL/squared_distance_2.h>
 
 #include <nlohmann/json.hpp>
 
@@ -20,20 +22,45 @@ typedef R::Point_2                       Point;
 typedef Creator_uniform_2<double,Point>  Creator;
 typedef std::vector<Point>               Vector;
 
-void generateRandomPoints(Vector& points, int num){
+
+/**
+ * Takes input points
+ * For each point
+ *      If point does not belong to an existing disc
+ *          Create a new disc
+ * Return the set of unit disc centers
+ */
+class Centers{
+    private:
+        Vector points;
+        Vector unitDiscCenters;
+        int unitSize;
+    
+    public:
+        Centers(){}
+
+        Centers(Vector points, int unitSize){
+            points = points;
+            unitSize = unitSize;
+        }
+        
+        void generateRandomPoints(int);
+        void loadFromFile();
+        void init();
+        void setUnitSize(int);
+        void showPoints();
+        void showUnitDiscCenters();
+        void findDiscCenters();
+};
+
+void Centers::generateRandomPoints(int num){
     points.reserve(num);
     Random_points_in_square_2<Point,Creator> g( 200.0);
     CGAL::cpp11::copy_n( g, num, std::back_inserter(points));
     cout << " Generated " << points.size() << " random points" << endl;
 }
 
-void showPoints(Vector& points){
-    for(long unsigned int i=0; i<points.size(); i++) {
-        cout << " Point[" << i << "] " << points[i].x() << ", " << points[i].y() << endl; 
-    }
-}
-
-void loadFromFile(Vector& points){
+void Centers::loadFromFile(){
     json jsonData;
     ifstream inputFile("data.json");
     inputFile >> jsonData;
@@ -44,16 +71,56 @@ void loadFromFile(Vector& points){
     }
 }
 
+void Centers::init(){
+    //start with the first point as a center for the first disc
+    unitDiscCenters.push_back(points[0]);
+}
+
+void Centers::setUnitSize(int num){
+    unitSize = num;
+}
+
+void Centers::showPoints(){
+    cout << endl << " [Centers] points" << endl; 
+    
+    for(long unsigned int i=0; i<points.size(); i++) {
+        cout << " Point[" << i << "] " << points[i].x() << ", " << points[i].y() << endl; 
+    }
+}
+
+void Centers::showUnitDiscCenters(){
+    cout << endl << " [Centers] unit disc centers" << endl; 
+    
+    for(long unsigned int i=0; i<unitDiscCenters.size(); i++) {
+        cout << " Unit Disc Center[" << i << "] " << unitDiscCenters[i].x() << ", " << unitDiscCenters[i].y() << endl; 
+    }
+}
+
+void Centers::findDiscCenters(){
+    cout << endl << " [Centers] findDiscCenters " << endl;
+
+    // for(long unsigned int i=0; i<points.size(); i++) {
+    //     //cout << " Point[" << i << "] " << points[i].x() << ", " << points[i].y() << endl; 
+    // }
+}
+
+
+
+
+
+
+
+
 /**
  * Psuedo Code
  * Create Points (either random or form file)
- * For each point
- *      If point does not belong to an existing disc
- *          Create a new disc
+ * Create new Centers object with data (or file)
+ * Call ob.findUnitDiskCover()
+ * Output to json file
  */
 int main(){
     int menuChoice, sampleSize, unitSize;
-    Vector points;
+    Centers obj;
 
     cout << endl;
     cout << " Unit Disk Covers - Brute Force " << endl;
@@ -68,18 +135,22 @@ int main(){
         case 1 :
             cout << endl << " How many points should be generated? : ";
             cin >> sampleSize;
-            generateRandomPoints(points, sampleSize);
+            obj.generateRandomPoints(sampleSize);
             break;
         case 2 : 
-            loadFromFile(points);
+            obj.loadFromFile();
             break;            
         case 3 : 
         default :
             return 0;
     }
 
-    showPoints(points);
-
     cout << endl << " What is the size of a unit? : ";
     cin >> unitSize;
+
+    obj.init();
+    obj.setUnitSize(unitSize);
+    obj.showPoints();
+    obj.showUnitDiscCenters();
+    obj.findDiscCenters();
 }
