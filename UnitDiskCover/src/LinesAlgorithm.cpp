0eff5443e74c4bbfbc01177821db3e6818134b85
ronaldshev <ronaldshevchenko1@gmail.com>
2018-11-22 01:20:12 -0500

    almost there

--- UnitDiskCover/src/LinesAlgorithm.cpp
+++ UnitDiskCover/src/LinesAlgorithm.cpp
@@ -9,27 +9,41 @@ LinesAlgorithm::LinesAlgorithm(vector<Point> &P, vector<Point> &C) {
                                     return (pi.x() < pj.x()) || ( pi.x() == pj.x() && pi.y() < pj.y());
                             });
     bool sorted = false;
-    double counter = 3;
+    double root = 3;
+    double constant = -19;
+    double counter = -39;
     int index = 0;
     while(sorted == false){
         for(Point p: P){
             index++;
-            if((p.x() <= sqrt(counter))){
-                Point topPoint(sqrt(counter)/2, p.y() - sqrt(1-pow((sqrt(counter)/2), 2)));
-                Point bottomPoint(sqrt(counter)/2, p.y() + sqrt(1-pow((sqrt(counter)/2), 2)));
+
+            if((p.x() <= constant*sqrt(root) && p.x() >= (constant - 1)*sqrt(root))){
+                Point onRestrLine((counter*sqrt(root)/2), p.y());
+                double distance = sqrt(squared_distance(p, onRestrLine));
+                double pointLength = sqrt(1-pow(distance, 2));
+
+                Point topPoint(onRestrLine.x(), onRestrLine.y() + pointLength);
+                Point bottomPoint(onRestrLine.x(), onRestrLine.y() - pointLength);
                 Segment seg(topPoint, bottomPoint);
                 Segments.push_back(seg);
                 //sort(P.begin(), index, [](const Point& pi, const Point& pj) {
                 //                                return pi.y() < pj.y();
                 //                  });
-                sorted = true;
             }
         }
-        for(Segment s: Segments){
-            C.push_back(pointlol);
-            //C.push_back(midpoint(s.max(), s.min()));
+        for(int i = 0; i < Segments.size(); i++){
+            if(Segments[i].min() > Segments[i+1].max()){
+                C.push_back(midpoint(Segments[i].min(), Segments[i+1].max()));
+            }else{
+                C.push_back(midpoint(Segments[i].min(), Segments[i].max()));
+                i++;
+            }
         }
-        counter *= 2;
+        constant++;
+        counter += 2;
+        if(constant*sqrt(root) > P.back().x()){
+            sorted = true;
+        }
 
     }
     cout << Segments.size() << endl;
